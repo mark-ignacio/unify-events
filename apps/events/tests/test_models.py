b39a20d06d442265a8f402f11562af4423da460c
Demetrius Ford <dford.py@knights.ucf.edu>
2015-06-22 14:52:35 -0400

    Tested event instances; tests will need to be refactored soon

--- apps/events/tests/test_models.py
+++ apps/events/tests/test_models.py
@@ -10,6 +10,7 @@ from core.models import TimeCreatedModified
 from events.models import Calendar
 from events.models import Category
 from events.models import Location
+from events.models import EventInstance
 from events.models import Event
 
 from faker import Factory
@@ -41,7 +42,7 @@ def test_main_calendar_is_identified():
     is_(calendar.is_main_calendar, msg=None)
 
 
-def test_that_calendar_can_have_an_owner():
+def test_calendar_has_an_owner():
     """
     Test that Calendar model can be owned by a `User` model.
     """
@@ -59,3 +60,44 @@ def test_that_calendar_can_have_an_owner():
     )
     # Is `user` an owner of Calendar? This should be `True`.
     is_(calendar.is_creator(user), msg=None)
+
+
+def test_calendar_retrieves_event_instances():
+    """
+    Test that Calendar model can retrieve all event instances.
+    """
+    fake = Factory.create()
+
+    user = User.objects.create_user(
+        username=fake.user_name(),
+        email=fake.email(),
+        password=fake.password()
+    )
+    calendar = Calendar.objects.create(
+        owner=user,
+        title=fake.text(max_nb_chars=64),
+        description=fake.text(max_nb_chars=140)
+    )
+    category = Category.objects.create(
+        title=fake.text(max_nb_chars=128)
+    )
+    # Event instances should initially be empty.
+    is_(len(list(calendar.event_instances)) == 0, msg=None)
+
+    event = Event.objects.create(
+        calendar=calendar,
+        creator=user,
+        title=fake.text(max_nb_chars=255),
+        description=fake.text(),
+        contact_name=fake.name(),
+        contact_email=fake.email(),
+        contact_phone=fake.phone_number(),
+        category=category
+    )
+    event_instance = EventInstance.objects.create(
+        event=event,
+        start=fake.date(pattern='%Y-%m-%d'),
+        end=fake.date(pattern='%Y-%m-%d')
+    )
+    # Event instances should now be equal to one.
+    is_(len(list(calendar.event_instances)) == 1, msg=None)
