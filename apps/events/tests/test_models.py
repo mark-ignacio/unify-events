103b40c51ff158e519e0bd273971ccc55fbd07c8
Demetrius Ford <dford.py@knights.ucf.edu>
2015-06-25 13:07:16 -0400

    Added Factory-Boy for model tests

--- apps/events/tests/test_models.py
+++ apps/events/tests/test_models.py
@@ -1,126 +1,162 @@
 """
-Run tests: `python manage.py test events`.
+Run tests: `python manage.py test events --verbosity=2 | grep ^Test.*`.
 """
 
 from nose.tools import ok_ as is_
 
+from django.test import TestCase
 from django.contrib.auth.models import User
 from core.models import TimeCreatedModified
+
+from factory import DjangoModelFactory
+from factory import SubFactory
 
 from events.models import Calendar
 from events.models import Category
 from events.models import Location
-from events.models import EventInstance
 from events.models import Event
+from events.models import EventInstance
 
 from faker import Factory
 
 
-class ModelFactory(object):
-
-    """Model Factory."""
-
-    @staticmethod
-    def factory(model):
-        fake = Factory.create()
-
-        if model == 'User':
-            return User.objects.create_user(
-                username=fake.user_name(),
-                email=fake.email(),
-                password=fake.password()
-            )
-        assert 0, 'Erroneous model: {0}'.format(model)
+faker = Factory.create()
 
 
-def generate(kind, how_many):
-    models = []
-    for i in range(how_many):
-        models.append(ModelFactory.factory(kind))
-    return models
+class UserFactory(DjangoModelFactory):
+
+    class Meta:
+        model = User
+
+    username = faker.user_name()
+    password = faker.password()
+    email = faker.email()
 
 
-def test_calendar_subclasses_time_created_modified():
-    """
-    Test that Calendar model is a subclass of `TimeCreatedModified`.
-    """
-    is_(issubclass(Calendar, TimeCreatedModified), msg=None)
+class CategoryFactory(DjangoModelFactory):
+
+    class Meta:
+        model = Category
+
+    title = faker.text(max_nb_chars=128)
 
 
-def test_calendar_has_callable_fields():
-    """
-    Test that Calendar model has "specific" callable fields.
-    """
-    fields = Calendar._meta.get_all_field_names()
-    labels = ['title', 'description', 'subscriptions']
-    clones = set(labels).intersection(fields)
-    is_(len(labels) == len(clones), msg=None)
+class CalendarFactory(DjangoModelFactory):
+
+    class Meta:
+        model = Calendar
+
+    title = 'Events at UCF'
+    owner = SubFactory(UserFactory)
 
 
-def test_main_calendar_is_identified():
-    """
-    Test that Main Calendar is identified by `FRONT_PAGE_CALENDAR_PK`.
-    """
-    calendar = Calendar.objects.create(title='Events at UCF')
-    is_(calendar.pk is not None, msg=None)
-    is_(calendar.is_main_calendar, msg=None)
+class EventFactory(DjangoModelFactory):
+
+    class Meta:
+        model = Event
+
+    calendar = SubFactory(CalendarFactory)
+    creator = SubFactory(UserFactory)
+
+    title = faker.text(max_nb_chars=255)
+    description = faker.text()
+
+    contact_name = faker.name()
+    contact_email = faker.email()
+    contact_phone = faker.phone_number()
+
+    category = SubFactory(CategoryFactory)
 
 
-def test_calendar_has_an_owner():
-    """
-    Test that Calendar model can be owned by a `User` model.
-    """
-    fake = Factory.create()
+class EventInstanceFactory(DjangoModelFactory):
 
-    users = generate('User', 2)
-    calendar = Calendar.objects.create(
-        owner=users[0],
-        title=fake.text(max_nb_chars=64),
-        description=fake.text(max_nb_chars=140)
-    )
-    # Is the user an owner of Calendar? This should be `True`.
-    is_(calendar.is_creator(users[0]), msg=None)
+    class Meta:
+        model = EventInstance
 
-    # What if given an arbitrary user? This should be `False`.
-    is_(calendar.is_creator(users[1]) == False, msg=None)
+    event = SubFactory(EventFactory)
+    start = str(faker.date_time_this_month(before_now=False, after_now=True))
+    end = str(faker.date_time_this_month(before_now=False, after_now=True))
 
 
-def test_calendar_retrieves_event_instances():
-    """
-    Test that Calendar model can retrieve all event instances.
-    """
-    fake = Factory.create()
+class TestEventModels(TestCase):
 
-    user = User.objects.create_user(
-        username=fake.user_name(),
-        email=fake.email(),
-        password=fake.password()
-    )
-    calendar = Calendar.objects.create(
-        owner=user,
-        title=fake.text(max_nb_chars=64),
-        description=fake.text(max_nb_chars=140)
-    )
-    category = Category.objects.create(
-        title=fake.text(max_nb_chars=128)
-    )
-    # Event instances should initially be empty.
-    is_(len(list(calendar.event_instances)) == 0, msg=None)
+    def setUp(self):
+        self.calendar = CalendarFactory()
 
-    event = Event.objects.create(
-        calendar=calendar,
-        creator=user,
-        title=fake.text(max_nb_chars=255),
-        description=fake.text(),
-        contact_name=fake.name(),
-        contact_email=fake.email(),
-        contact_phone=fake.phone_number(),
-        category=category
-    )
-    event_instance = EventInstance.objects.create(
-        event=event,
-        start=fake.date(pattern='%Y-%m-%d'),
-        end=fake.date(pattern='%Y-%m-%d')
-    )
-    # Event instances should now be equal to one.
-    is_(len(list(calendar.event_instances)) == 1, msg=None)
+    def tearDown(self):
+        self.calendar.delete()
+
+    def test_calendar_responds_to_fields(self):
+        """
+        Test that Calendar model responds to callable fields.
+        """
+        fields = self.calendar._meta.get_all_field_names()
+        labels = ['title', 'description', 'subscriptions']
+        clones = set(labels).intersection(fields)
+        is_(len(labels) == len(clones), msg=None)
+
+    def test_main_calendar_is_identified(self):
+        """
+        Test that Main Calendar is identified by ``FRONT_PAGE_CALENDAR_PK``.
+        """
+        is_(self.calendar.pk is not None, msg=None)
+        is_(self.calendar.is_main_calendar, msg=None)
+
+    def test_calendar_has_an_owner(self):
+        """
+        Test that Calendar model can be owned by a `User` model.
+        """
+        # Is the user an owner of Calendar? This should be ``True``.
+        is_(self.calendar.owner is not None, msg=None)
+        is_(self.calendar.is_creator(self.calendar.owner), msg=None)
+
+        # What if given an arbitrary user? This should be ``False``.
+        random_user = UserFactory(
+            username='404_everywhere',
+            password='r3@lSecure',
+            email='rodneyrowe@crazespaces.pw'
+        )
+        is_(self.calendar.is_creator(random_user) == False, msg=None)
+        random_user.delete()
+
+    def test_calendar_retrieves_event_instances(self):
+        """
+        Test that Calendar model can retrieve all event instances.
+        """
+        user = UserFactory(
+            username='dylonmackrvr',
+            password='qwerty',
+            email='dylonmackAg3@crazespaces.pw'
+        )
+        calendar = CalendarFactory(
+            title='Knightsec Events',
+            owner=user,
+            description='Practices, meetings, and CTFs'
+        )
+        category = CategoryFactory(
+            title='Meeting'
+        )
+        # Event instances should initially be empty.
+        is_(len(list(calendar.event_instances)) == 0, msg=None)
+
+        meeting = EventFactory(
+            calendar=calendar,
+            creator=user,
+            title='Pick All the Locks',
+            description='"Who just leveled up?!"',
+            category=category
+        )
+        # Check that Calendar now contains the event.
+        is_(len(list(calendar.events.all())) != [])
+
+        instance = EventInstanceFactory(event=meeting)
+        # Event instances should now be equal to one.
+        is_(len(list(calendar.event_instances)) == 1, msg=None)
+
+        # Cleanup.
+        meeting.delete()
+        instance.delete()
+        user.delete()
+
+        calendar.delete()
+        category.delete()
