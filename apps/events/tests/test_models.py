0eaf5555f6b19501325256c3874452350f7fb7fd
Demetrius Ford <dford.py@knights.ucf.edu>
2015-06-25 15:35:51 -0400

    Tested calendar subscriptions and did some more refactoring

--- apps/events/tests/test_models.py
+++ apps/events/tests/test_models.py
@@ -15,10 +15,39 @@ from .factories import EventInstanceFactory
 class TestEventModels(TestCase):
 
     def setUp(self):
-        self.calendar = CalendarFactory()
+        """
+        Create initial model objects.
+        """
+        self.user = UserFactory(
+            username='dylonmackrvr',
+            password='qwerty',
+            email='dylonmackAg3@crazespaces.pw'
+        )
+        self.calendar = CalendarFactory(
+            title='Knightsec Events',
+            owner=self.user,
+            description='Practices, meetings, and CTFs'
+        )
+        self.category = CategoryFactory(
+            title='Meeting'
+        )
+        self.event = EventFactory(
+            calendar=self.calendar,
+            creator=self.user,
+            title='Pick All the Locks',
+            description='"Who just leveled up?!"',
+            category=self.category
+        )
 
     def tearDown(self):
+        """
+        Delete model objects.
+        """
+        self.user.delete()
         self.calendar.delete()
+        self.category.delete()
+
+        self.event.delete()
 
     def test_calendar_responds_to_fields(self):
         """
@@ -57,40 +86,37 @@ class TestEventModels(TestCase):
         """
         Test that Calendar model can retrieve all event instances.
         """
-        user = UserFactory(
-            username='dylonmackrvr',
-            password='qwerty',
-            email='dylonmackAg3@crazespaces.pw'
-        )
-        calendar = CalendarFactory(
-            title='Knightsec Events',
-            owner=user,
-            description='Practices, meetings, and CTFs'
-        )
-        category = CategoryFactory(
-            title='Meeting'
-        )
         # Event instances should initially be empty.
-        is_(len(list(calendar.event_instances)) == 0, msg=None)
+        is_(len(list(self.calendar.event_instances)) == 0, msg=None)
 
-        meeting = EventFactory(
-            calendar=calendar,
-            creator=user,
-            title='Pick All the Locks',
-            description='"Who just leveled up?!"',
-            category=category
-        )
         # Check that Calendar now contains the event.
-        is_(len(list(calendar.events.all())) != [])
+        is_(len(list(self.calendar.events.all())) != [], msg=None)
 
-        instance = EventInstanceFactory(event=meeting)
+        instance_of_event = EventInstanceFactory(event=self.event)
         # Event instances should now be equal to one.
-        is_(len(list(calendar.event_instances)) == 1, msg=None)
+        is_(len(list(self.calendar.event_instances)) == 1, msg=None)
 
-        # Cleanup.
-        meeting.delete()
-        instance.delete()
-        user.delete()
+    def test_calendar_subscribes_to_events(self):
+        """
+        Tests that Calendar model can subscribe and unsubscribe from events.
+        """
+        personal_calendar = CalendarFactory(
+            title='Personal Events',
+            owner=self.user,
+            description='Crash dummy'
+        )
+        instance_of_event = EventInstanceFactory(event=self.event)
 
-        calendar.delete()
-        category.delete()
+        # Let's subscribe to Knightsec's calendar model.
+        personal_calendar.subscriptions.add(self.calendar)
+        self.calendar.copy_future_events(personal_calendar)
+
+        # Have we subscribed to Knightsec? This should be ``True``.
+        is_(len(list(self.calendar.subscribing_calendars)) == 1, msg=None)
+
+        # What about unsubscribing? Knightsec won't be too happy.
+        personal_calendar.subscriptions.remove(self.calendar)
+        personal_calendar.delete_subscribed_events(self.calendar)
+
+        # Now, Knightsec shouldn't have any more subscriptions.
+        is_(len(list(self.calendar.subscribing_calendars)) == 0, msg=None)
